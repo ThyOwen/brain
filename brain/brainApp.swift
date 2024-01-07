51dd178356b75262e47c6cea72549280c37b4c2d
Owen O'Malley <theowen@email.com>
2024-01-07 13:39:49 -0500

    Initial Commit

---
+++ brain/brainApp.swift
@@ -0,0 +1,17 @@
+//
+//  brainApp.swift
+//  brain
+//
+//  Created by Owen O'Malley on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct brainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
