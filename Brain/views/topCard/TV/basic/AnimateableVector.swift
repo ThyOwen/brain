ca931f0c2a5b3f718143d96a1676b5bcd98d6e13
ThyOwen <148803620+ThyOwen@users.noreply.github.com>
2024-04-30 23:24:30 -0400

    Got the KarenVis working

---
+++ Brain/views/topCard/TV/basic/AnimateableVector.swift
@@ -0,0 +1,8 @@
+//
+//  AnimateableVector.swift
+//  Brain
+//
+//  Created by Owen O'Malley on 4/30/24.
+//
+
+import Foundation
---
+++ Brain/views/topCard/TV/basic/KarenWave.swift
@@ -0,0 +1,16 @@
+//
+//  KarenWave.swift
+//  Brain
+//
+//  Created by Owen O'Malley on 4/29/24.
+//
+
+import SwiftUI
+
+struct KarenWave: Path {
+    
+}
+
+#Preview {
+    KarenWave()
+}
